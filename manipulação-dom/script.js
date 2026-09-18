acd836486648acc5b53830c366b56f1d2ad54fa2
VINICIUS MANIEZO ALVES <42972208811@info.local>
2026-09-18 15:01:22 -0300

    :rocket: Finalizado manipulação de DOM

--- manipulação-dom/script.js
+++ manipulação-dom/script.js
@@ -72,14 +72,53 @@ elemento.addEventListener("mouseout", () => {
 
 //? Evento de formulario (submit)
 
-const form = document.querySelector("form") //* Pegando pela tag <form>
+// const form = document.querySelector("form") //* Pegando pela tag <form>
 
 //* O comportamento padrão do formulario é recarregar a pagina ao enviar o default
-form.addEventListener("submit", (e) =>  {
-    e.preventDefault(); //* Bloqueia o comportamento padrão do formulario
+// form.addEventListener("submit", (e) =>  {
+//     e.preventDefault(); //* Bloqueia o comportamento padrão do formulario
 
-    const nome = document.getElementById("nome").value
+//     const nome = document.getElementById("nome").value
 
-    console.log(`Nome: ${nome}`);
+//     console.log(`Nome: ${nome}`);
     
+// })
+
+//? Criando elementos na página
+// const novoElemento = document.createElement("p"); //* Cria o elemento <p>
+// novoElemento.innerText = "Elemento novo criado."; //* Cria um texto no elemento
+
+// const form = document.getElementById("form");
+
+
+// form.appendChild(novoElemento); //* Atribui um elemento filho (<p>) dentro do pai (<form>)
+
+// const button = document.createElement("button");
+// button.innerText = "Excluir elemento";
+// form.appendChild(button);
+
+// button.addEventListener("click", (e) => {
+//     e.preventDefault();
+
+//     novoElemento.remove();
+
+     //* Apagando direto do pai
+     // form.removeChild(novoElemento);
+// })
+
+//? Adicionando elementos em uma lista via <input>
+
+const input = document.getElementById("input"); // Input
+const button = document.getElementById("add"); // Botao
+const lista = document.getElementById("lista"); // ul
+
+button.addEventListener("click", () => {
+    const valorDigitado = input.value; //* Armazena oque é digitado dentro do input
+
+    const li = document.createElement("li"); //* Criando <li>
+    li.innerText = valorDigitado;
+
+    lista.appendChild(li); //* Colocando <li> dentro do pai <ul>
+
+    input.value = ""
 })
